dacfc805c2c4907710d8d4f37f7c5422cbc3c2dd
SolidHinken <solidhinken@gmail.com>
2020-03-04 13:43:25 +0300

    04.03.20
    Enemies spawner
    Sword swinger

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.1f;
+    private Rigidbody rb;
+
+    private bool WasHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!WasHit)
+            transform.Translate(transform.forward  * speed);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            WasHit = true;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddForce(Vector3.right * Random.Range(-2f,1f)*15f+ Vector3.up * 700f);
+        }
+        else if(collision.transform.CompareTag("Water"))
+        {
+            Destroy(gameObject, 1f);
+        }
+    }
+
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPref;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            InvokeRepeating(nameof(SpawnEnemyWave),2f,5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+        }
+    }
+
+
+    public void SpawnEnemyWave()
+    {
+        for (int i = 0; i < Random.Range(1,10); i++)
+        {
+            Instantiate(enemyPref, transform.position + Vector3.right*(Random.Range(-2f,1f)),Quaternion.AngleAxis(180f,Vector3.up),transform);
+        }
+    }
+
+}
